eeaa0b8a8beb3a928c9c20faad4a19252db278f5
Pierre Bürki <pierre.burki@he-arc.ch>
2020-02-26 15:06:31 +0100

    Create Spring Boot project

---
+++ src/main/java/ch/hearc/todont/ToDontApplication.java
@@ -0,0 +1,13 @@
+package ch.hearc.todont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDontApplication.class, args);
+	}
+
+}
