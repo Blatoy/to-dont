60d9eef7bd5e21cbe82b1c78f899be592e539a5b
David Rihs <david.rihs@he-arc.ch>
2020-04-20 17:05:08 +0200

    replace var by let

--- src/main/resources/static/js/create.js
+++ src/main/resources/static/js/create.js
@@ -1,25 +1,25 @@
 function removeUser(row) {
-    var i = row.parentNode.parentNode.rowIndex;
+    let i = row.parentNode.parentNode.rowIndex;
     document.getElementById('users-table').deleteRow(i);
 }
 
 
 function addUser() {
-    var table = document.getElementById('users-table');
-    var tr = table.insertRow(-1);
+    let table = document.getElementById('users-table');
+    let tr = table.insertRow(-1);
 
     tr.innerHTML = '<td><label><input class="uk-checkbox" type="checkbox"> Moderator</label></td>\
                     <td><button class="uk-button uk-button-small uk-close-large remove-user-button" onclick="removeUser(this)" type="button" uk-close></button></td>';
 
 
 
-    var input = document.createElement('input');
+    let input = document.createElement('input');
     input.required = true;
     input.setAttribute('type', 'input');
     input.setAttribute('placeholder', 'Name');
     input.className = "uk-input"
 
-    var td = tr.insertCell(0);
+    let td = tr.insertCell(0);
     td.appendChild(input);
 
     input.focus();
