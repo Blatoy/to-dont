5eed89ce294e70f288aed2e35c2265f46741107c
Pierre Bürki <pierre.burki@he-arc.ch>
2020-04-20 18:50:57 +0200

    Fix findByVisibility

--- src/main/java/ch/hearc/todont/repositories/ToDontRepository.java
+++ src/main/java/ch/hearc/todont/repositories/ToDontRepository.java
@@ -4,9 +4,10 @@ import ch.hearc.todont.models.ToDont;
 import ch.hearc.todont.models.ToDont.Visibility;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ToDontRepository extends JpaRepository<ToDont, UUID> {
 
-    Page<ToDont> findByVisibility(Visibility visibility);
+    Page<ToDont> findByVisibility(Visibility visibility, Pageable pageable);
 }
