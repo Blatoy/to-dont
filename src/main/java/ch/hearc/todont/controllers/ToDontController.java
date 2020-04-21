902c0ae1fdb5e0e8b7a4f5e4de6d5548610f5537
Pierre Bürki <pierre.burki@he-arc.ch>
2020-04-21 21:17:05 +0200

    Fix User acquisition in controllers (ugly)

--- src/main/java/ch/hearc/todont/controllers/ToDontController.java
+++ src/main/java/ch/hearc/todont/controllers/ToDontController.java
@@ -2,6 +2,7 @@ package ch.hearc.todont.controllers;
 
 import ch.hearc.todont.models.ToDont;
 import ch.hearc.todont.models.User;
+import ch.hearc.todont.repositories.UserRepository;
 import ch.hearc.todont.services.CommentService;
 import ch.hearc.todont.services.ToDontService;
 import java.util.UUID;
@@ -24,18 +25,24 @@ public class ToDontController {
     @Autowired
     private CommentService commentService;
 
+    @Autowired
+    private UserRepository userRepo;
+
     /**
      * GET on a specific ToDont page.
      * 
      * @param toDontId UUID of the ToDont
-     * @param user User requesting the page
+     * @param userDetail User requesting the page
      * @param model Model passed down to the view
      * @return The page template name
      */
     @GetMapping("/{toDontId}")
     public String toDont(
         @PathVariable("toDontId") UUID toDontId,
-        @AuthenticationPrincipal User user, Model model) {
+        @AuthenticationPrincipal org.springframework.security.core.userdetails.User userDetail,
+        Model model) {
+
+        User user = userRepo.findByName(userDetail.getUsername());
 
         ToDont toDont = toDontService.getToDont(user, toDontId);
         if (toDont != null) {    
@@ -51,15 +58,18 @@ public class ToDontController {
      * POST on the /{id}/pledge, used to pledge to a ToDont.
      * 
      * @param toDontId UUID of the ToDont
-     * @param user User joining the ToDont
+     * @param userDetail User joining the ToDont
      * @return The page template name
      */
     @PostMapping("/{toDontId}/pledge")
     public String pledge(
         @PathVariable("toDontId") UUID toDontId,
-        @AuthenticationPrincipal User user,
+        @AuthenticationPrincipal org.springframework.security.core.userdetails.User userDetail,
         Model model
     ) {
+
+        User user = userRepo.findByName(userDetail.getUsername());
+
         ToDont toDont = toDontService.getToDont(user, toDontId);
         if (toDont != null) {
             toDontService.pledgeToToDont(user, toDont);
@@ -74,15 +84,18 @@ public class ToDontController {
      * POST on the /{id}/fail, used to fail a ToDont.
      * 
      * @param toDontId UUID of the ToDont
-     * @param user User failing the ToDont
+     * @param userDetail User failing the ToDont
      * @return The page template name
      */
     @PostMapping("/{toDontId}/fail")
     public String fail(
         @PathVariable("toDontId") UUID toDontId,
-        @AuthenticationPrincipal User user,
+        @AuthenticationPrincipal org.springframework.security.core.userdetails.User userDetail,
         Model model
     ) {
+
+        User user = userRepo.findByName(userDetail.getUsername());
+
         ToDont toDont = toDontService.getToDont(user, toDontId);
         if (toDont != null) {
             toDontService.failToDont(user, toDont);
@@ -95,17 +108,20 @@ public class ToDontController {
      * POST on /{id}/comment, used to post comments on a ToDont.
      * 
      * @param toDontId UUID of the ToDont
-     * @param user User posting the comment
+     * @param userDetail User posting the comment
      * @param comment Content of the comment
      * @return The page template name
      */
     @PostMapping("/{toDontId}/comment")
     public String comment(
         @PathVariable("toDontId") UUID toDontId,
-        @AuthenticationPrincipal User user,
+        @AuthenticationPrincipal org.springframework.security.core.userdetails.User userDetail,
         @RequestParam(name = "comment") String comment,
         Model model
     ) {
+
+        User user = userRepo.findByName(userDetail.getUsername());
+
         ToDont toDont = toDontService.getToDont(user, toDontId);
         if (toDont != null) {
             if (commentService.post(user, toDont, comment)) {
@@ -124,17 +140,20 @@ public class ToDontController {
      * DELETE on /{id}/comment, used to delete a comment on a ToDont.
      * 
      * @param toDontId UUID of the ToDont
-     * @param user User deleting the comment
+     * @param userDetail User deleting the comment
      * @param commentId ID of the comment
      * @return The page template name
      */
     @DeleteMapping("/{toDontId}/comment")
     public String deleteComment(
         @PathVariable("toDontId") UUID toDontId,
-        @AuthenticationPrincipal User user,
+        @AuthenticationPrincipal org.springframework.security.core.userdetails.User userDetail,
         @RequestParam(name = "commentId") long commentId,
         Model model
     ) {
+
+        User user = userRepo.findByName(userDetail.getUsername());
+
         ToDont toDont = toDontService.getToDont(user, toDontId);
         if (toDont != null) {
             commentService.delete(user, commentId);
