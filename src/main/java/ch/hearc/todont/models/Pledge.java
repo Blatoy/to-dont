1dcc3a8e1af1387045ea641720b557ba64ffa22e
Pierre Bürki <pierre.burki@he-arc.ch>
2020-04-21 21:11:34 +0200

    Clean up Pledge class

--- src/main/java/ch/hearc/todont/models/Pledge.java
+++ src/main/java/ch/hearc/todont/models/Pledge.java
@@ -82,7 +82,6 @@ public class Pledge {
     }
 
     public void setId(PledgeKey id) {
-        System.out.println("Setting pledgeKey to " + id);
         this.id = id;
     }
 
@@ -116,36 +115,5 @@ public class Pledge {
 
     public void setDateFailed(Timestamp dateFailed) {
         this.dateFailed = dateFailed;
-    }
-
-    @Override
-    public String toString() {
-        return "Pledge [dateFailed=" + dateFailed + ", dateJoined=" + dateJoined + ", id=" + id + ", toDont=" + toDont
-                + ", user=" + user + "]";
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Pledge other = (Pledge) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
     }
 }
