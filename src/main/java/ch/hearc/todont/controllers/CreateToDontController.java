478f708f4ae2e93b766ca06ebcf9da152d6d9292
Tristan Seuret <tristan.seuret@he-arc.ch>
2020-04-21 16:15:07 +0200

    Corrected G-Wizzy comments

--- src/main/java/ch/hearc/todont/controllers/CreateToDontController.java
+++ src/main/java/ch/hearc/todont/controllers/CreateToDontController.java
@@ -4,25 +4,18 @@ import ch.hearc.todont.models.ToDont;
 import ch.hearc.todont.models.User;
 import ch.hearc.todont.repositories.ToDontRepository;
 import ch.hearc.todont.services.ToDontService;
-
 import java.sql.Timestamp;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.util.Pair;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/todont/create")
 public class CreateToDontController {
 
     @Autowired
@@ -37,7 +30,7 @@ public class CreateToDontController {
      * @param model Model passed down to the view
      * @return The page template name
      */
-    @GetMapping("/")
+    @GetMapping("/create")
     public String get(Model model) {
         ToDont toDont = new ToDont();
         model.addAttribute("toDont", toDont);
@@ -51,13 +44,14 @@ public class CreateToDontController {
      * @param newToDont Returned value from form
      * @return The page template name
      */
-    @PostMapping("/")
+    @PostMapping("/create")
     public String createToDont(@RequestParam(name = "usernames[]") String[] usernames, 
             @RequestParam(name= "moderatorBoolean[]") String[] moderators,
             @AuthenticationPrincipal User user,
             @ModelAttribute ToDont newToDont) {
 
-        newToDont.setDatePublished(Timestamp.from(Instant.now());
+        newToDont.setDatePublished(Timestamp.from(Instant.now()));
+        newToDont.setOwner(user);
 
         // Add user by username to todont by pledge
         for (int i = 0; i < usernames.length; i++) {    
