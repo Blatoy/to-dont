0e80ee8a72a32a18b603f94b1cdfae182841b689
Blatoy <lock.jeanneret@gmail.com>
2020-04-21 11:51:40 +0200

    Add email validator
    Co-authored-by: Pierre Bürki <54360836+g-wizzy@users.noreply.github.com>

---
+++ src/main/java/ch/hearc/todont/validators/ValidEmail.java
@@ -0,0 +1,32 @@
+package ch.hearc.todont.validators;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Target({ ElementType.TYPE, ElementType.FIELD, ElementType.ANNOTATION_TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = EmailValidator.class)
+@Documented
+public @interface ValidEmail {   
+    /**
+     * Default error message.
+     * @return
+     */
+    String message() default "Invalid email";
+
+    /**
+     * Default groups.
+     * @return
+     */
+    Class<?>[] groups() default {}; 
+
+    /**
+     * Default payload.
+     */
+    Class<? extends Payload>[] payload() default {};
+}
