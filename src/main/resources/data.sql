1e1562d53f0c94f60d5a646f1f66005bd0aa4074
David Rihs <david.rihs@he-arc.ch>
2020-04-21 21:18:02 +0200

    link navbar

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO USERS (EMAIL ,NAME ,PASSWORD)
+VALUES
+    ('toto@gmail.com','Toto','$2a$04$yeE1UkQ8guWcidsrNVIGGebBrqF8bzMzLNYQbFkgztqOhWX.1GsF2'),
+    ('titi@gmail.com','Titi','$2a$04$yeE1UkQ8guWcidsrNVIGGebBrqF8bzMzLNYQbFkgztqOhWX.1GsF2');
