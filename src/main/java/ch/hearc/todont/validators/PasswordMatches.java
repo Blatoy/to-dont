f0ebf48cc73dfa1bd9372e05ee465ed18b9639b9
Blatoy <lock.jeanneret@gmail.com>
2020-04-21 11:51:53 +0200

    Add password validator
    Co-authored-by: Pierre Bürki <54360836+g-wizzy@users.noreply.github.com>

---
+++ src/main/java/ch/hearc/todont/validators/PasswordMatches.java
@@ -0,0 +1,33 @@
+package ch.hearc.todont.validators;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE}) 
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PasswordValidator.class)
+@Documented
+public @interface PasswordMatches { 
+    /**
+     * Default error message.
+     * @return
+     */
+    String message() default "Passwords don't match";
+    
+    /**
+     * Default group.
+     * @return
+     */
+    Class<?>[] groups() default {}; 
+    
+    /**
+     * Default payload.
+     * @return
+     */
+    Class<? extends Payload>[] payload() default {};
+}
