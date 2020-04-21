79c24ea71c54a6c68407cb4738d72c7f9a61cca4
Blatoy <lock.jeanneret@gmail.com>
2020-04-21 11:53:24 +0200

    Add registration
    Co-authored-by: Pierre Bürki <54360836+g-wizzy@users.noreply.github.com>

---
+++ src/main/java/ch/hearc/todont/controllers/RegistrationController.java
@@ -0,0 +1,49 @@
+package ch.hearc.todont.controllers;
+
+import ch.hearc.todont.datatransfer.UserDto;
+import ch.hearc.todont.models.User;
+import ch.hearc.todont.services.UserService;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+public class RegistrationController {
+
+    @Autowired
+    private UserService userService;
+    
+    /**
+     * GET register.
+     */
+    @GetMapping("/register")
+    public String registerForm(Model model) {
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
+        return "register";
+    }
+    
+    /**
+     * POST register.
+     */
+    @PostMapping("/register")
+    public String register(
+        @ModelAttribute("user") @Valid UserDto userDto,
+        Model model
+    ) {
+        try {
+            User registered = userService.registerUser(userDto);
+
+            model.addAttribute("user", registered);
+            return "redirect:home";
+        } catch (Exception ex) {
+            model.addAttribute("message", "This email is already used.");
+            return "register";
+        }   
+    }
+}
