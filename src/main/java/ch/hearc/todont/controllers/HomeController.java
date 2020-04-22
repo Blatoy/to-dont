070137444cd8fb1caa413f38decae3ac52c33bec
David Rihs <david.rihs@he-arc.ch>
2020-04-22 16:02:53 +0200

    Pagination

--- src/main/java/ch/hearc/todont/controllers/HomeController.java
+++ src/main/java/ch/hearc/todont/controllers/HomeController.java
@@ -1,9 +1,16 @@
 package ch.hearc.todont.controllers;
 
 import ch.hearc.todont.models.User;
+import ch.hearc.todont.models.ToDont;
 import ch.hearc.todont.repositories.UserRepository;
 import ch.hearc.todont.services.ToDontService;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -32,23 +39,41 @@ public class HomeController {
     public String home(
         @AuthenticationPrincipal org.springframework.security.core.userdetails.User userDetail,
         Model model,
-        @RequestParam(defaultValue = "0") int myToDontsPage,
-        @RequestParam(defaultValue = "0") int publicToDontsPage) {
+        @RequestParam(name="myToDontsPage", defaultValue = "1") int myToDontsPage,
+        @RequestParam(name="publicToDontsPage", defaultValue = "1") int publicToDontsPage) {
         
         User user = userRepo.findByName(userDetail.getUsername());
+        
+        myToDontsPage -= 1;
+        publicToDontsPage -= 1;
 
-        model.addAttribute("myToDonts", toDontService.getUserToDonts(
+        Page<ToDont> myToDonts = toDontService.getUserToDonts(
             user,
             myToDontsPage,
             myToDontsPerPage
-        ));
-        model.addAttribute("publicToDonts", toDontService.getPublicToDonts(
+        );
+        
+        List<Integer> myTodontsPageNumbers = IntStream.rangeClosed(1, myToDonts.getTotalPages())
+            .boxed()
+            .collect(Collectors.toList());
+        model.addAttribute("myToDontsPageNumbers", myTodontsPageNumbers);
+        
+
+        Page<ToDont> publicToDonts = toDontService.getPublicToDonts(
             publicToDontsPage,
             publicToDontsPerPage
-        ));
+        );
+
+        List<Integer> publicToDontsPageNumbers = IntStream.rangeClosed(1, publicToDonts.getTotalPages())
+            .boxed()
+            .collect(Collectors.toList());
+        model.addAttribute("publicToDontsPageNumbers", publicToDontsPageNumbers);
+
+        model.addAttribute("myToDonts", myToDonts);
+        model.addAttribute("publicToDonts", publicToDonts);
         return "home";
     }
 
-    static final int myToDontsPerPage = 5;
-    static final int publicToDontsPerPage = 5;
+    static final int myToDontsPerPage = 3;
+    static final int publicToDontsPerPage = 3;
 }
