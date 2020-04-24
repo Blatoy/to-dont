4c72d0a293a66fc3699ad82dae81325cd3d05795
Pierre Bürki <pierre.burki@he-arc.ch>
2020-04-24 11:07:15 +0200

    Add missing pledge unit test

--- src/test/java/ch/hearc/todont/models/PledgeTest.java
+++ src/test/java/ch/hearc/todont/models/PledgeTest.java
@@ -1,5 +1,6 @@
 package ch.hearc.todont.models;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import ch.hearc.todont.models.ToDont.Visibility;
@@ -17,5 +18,29 @@ public class PledgeTest {
         pledge.fail();
 
         assertTrue(pledge.isFailed());
+    }
+
+    @Test
+    public void givenAModeratorPledge_whenCallisModerator_thenReturnTrue() {
+        User owner = new User();
+        User mod = new User();
+
+        ToDont toDont = new ToDont(owner, "Test", Visibility.PUBLIC);
+        Pledge pledge = new Pledge(mod, toDont);
+
+        toDont.addModerator(owner, mod);
+
+        assertTrue(pledge.isModerator());
+    }
+
+    @Test
+    public void givenANonModeratorPledge_whenCallisModerator_thenReturnFalse() {
+        User owner = new User();
+        User mod = new User();
+
+        ToDont toDont = new ToDont(owner, "Test", Visibility.PUBLIC);
+        Pledge pledge = new Pledge(mod, toDont);
+
+        assertFalse(pledge.isModerator());
     }
 }
